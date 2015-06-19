89d8deafef2c22e9fccdedfac002c235931f9623
Igor Serpak <igor.serpak@gmail.com>
2015-06-19 15:30:09 -0300

    Changed comment content to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619083743) do
+ActiveRecord::Schema.define(version: 20150619182033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string   "content"
+    t.text     "content"
     t.integer  "user_id"
     t.integer  "link_id"
     t.datetime "created_at", null: false
